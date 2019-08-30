4f33cc7a26ee63ac784290112b88ede06f747960
Casie Siekman <casie.siekman@gmail.com>
2019-08-30 16:21:23 -0500

    Updated with code from inputs lecture

--- script.js
+++ script.js
@@ -27,11 +27,51 @@ $(document).ready(onReady);
 
 function onReady(){
     listOutFriends();
+    $('#submitBtn').on('click',  handleSubmit);
 }
 
+// use .val() in some way, to clear out the inputs upon submit
+
+function handleSubmit(){
+    let nameValue = $('#name').val();
+    let food = $('#food').val();
+    let meal = $('#meal').val();
+
+    // if the NAME value is an empty string, show an alert
+    if(nameValue === ''){
+        $('#name').addClass('error');
+        alert("Please fill out the name input");
+    } else if(food === ''){
+        $('#food').addClass('error');
+        alert("Please fill out the food input");
+    } else if(meal === ''){
+        $('#meal').addClass('error');
+        alert("Please fill out the meal input");
+    } else {
+        // get rid of the border
+        $("#name").removeClass('error');
+        $("#meal").removeClass('error');
+        $("#food").removeClass('error');
+        
+        let newObject = {
+            name: nameValue,
+            favoriteMeal: meal,
+            favoriteFood: food
+        }
+
+        shadesOfCasie.push(newObject);
+
+        listOutFriends();
+
+        $('#name').val('');
+        $('#food').val('');
+        $('#meal').val('');
+    }
+}
 
 function listOutFriends(){
-for(person of shadesOfCasie){
+    $('#listFriends').empty();
+    for(person of shadesOfCasie){
         let $li = $(
             `<li data-cohort="Porta">
                 ${person.name}
@@ -47,7 +87,7 @@ for(person of shadesOfCasie){
 }
 
 function handleClick(){
-    let favFood = $(this).parent().data('favoriteFood');
+    let favFood = $(this).parent().data();
     console.log('this is a secret favorite food: ',  favFood);
-    $(this).parent().toggleClass('yellow');
+    $(this).toggleClass('yellow');
 }
